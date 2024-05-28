5b56cc1357f470d3c1b8a4ff4a150c4ea9abf1e6
hinarazaulmustafa <154458368+hinarazaulmustafa@users.noreply.github.com>
2024-05-29 03:38:50 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,85 @@
+// 1. Declare a variable called username.
+
+var username="abc"
+
+console.log(username);
+
+
+// 2. Declare a variable called myName & assign to it a string
+// that represents your Full Name.
+
+var myname ="hina mustafa"
+
+console.log(myname)
+
+
+// 3. Write script to
+// a) Declare a JS variable, titled message.
+// b) Assign “Hello World” to variable message
+// c) Display the message in alert box.
+
+var mynewmessage ="Hello World"
+
+alert(mynewmessage)
+
+
+// 4. Write a script to save student’s bio data in JS variables and
+// show the data in alert boxes.
+
+
+var myage= prompt("students age",15)
+
+var mycourse ="web developement"
+
+alert(myname)
+alert(myage)
+alert(mycourse)
+
+
+// 5.Write a script to display the following alert using one JS
+// variable:
+
+var myfood = "pizza\npizz\npiz\npi\np"
+
+alert(myfood)
+
+
+// 6. Declare a variable called email and assign to it a string that
+// represents your Email Address(e.g. example@example.com).
+// Show the blow mentioned message in an alert box.(Hint: use
+// string concatenation)
+
+var email ="example@example.com"
+
+var myemail= "my email address is"
+
+
+alert(myemail + email)
+
+
+// 7. Declare a variable called book & give it the value “A
+// smarter way to learn JavaScript”. Display the following
+// message in an alert box:
+
+
+var book =" A Smarter Way To Learn JavaScript"
+
+
+
+
+alert("i m learning from the book"+book)
+
+
+// 8. Write a script to display this in browser through JS
+
+
+document.write("Yah! i can write html content through javascript")
+
+// 9. Store following string in a variable and show in alert and
+// browser through JS
+// “▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”
+
+
+var character =("“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”")
+
+alert(character)
